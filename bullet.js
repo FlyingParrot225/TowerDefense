9b687b5f176ec9eb409065eab5350bb34b9b6a02
Garrett Yeno Jr <garrett.yenojr2005@gmail.com>
2020-10-15 10:11:02 -0400

    commit

--- bullet.js
+++ bullet.js
@@ -2,8 +2,6 @@ class Bullet{
     constructor(x,y,dx,dy){
         this.x = x;
         this.y = y;
-        this.dx = dx;
-        this.dy = dy;
         this.radius = 10;
         this.bulletSpeed = 1;
     }
