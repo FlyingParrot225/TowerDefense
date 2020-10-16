9d540466da6c8b04d6f4bde934182f139707e9f3
FlyingParrot225 <61127422+FlyingParrot225@users.noreply.github.com>
2020-10-16 10:07:47 -0400

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -1,24 +1,27 @@
 class Bullet{
-    constructor(x,y,dx,dy){
+    constructor(x,y,dx){
         this.x = x;
         this.y = y;
         this.radius = 10;
         this.bulletSpeed = 1;
         this.dx = dx;
-        this.dy = dy;
+        this.angle = -findAngle(mouse.x,mouse.y,this.x, this.y);
     }
     draw(){
+        ctx.save()
+        ctx.translate(this.x,this.y);
+        ctx.rotate(this.angle);
         ctx.beginPath();
         ctx.fillStyle = "yellow";
         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
         ctx.fill();
+        ctx.restore();
     }
     behaviour(){
         
     }
     update(){
         this.x += this.bulletSpeed*this.dx;
-        this.y += this.bulletSpeed*this.dy;
         this.draw();
         this.behaviour();
     }
